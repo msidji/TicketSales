8a677fa5135068951e4c577ac396ee23497d8d43
Milos Sidji <milos.sidji@gmail.com>
2021-05-27 16:12:12 +0200

    Add Validator for CreateConcertRequest

---
+++ src/TicketSales.Core/TicketSales.Core.Application/Features/Concerts/Commands/CreateConcert/CreateConcertRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace TicketSales.Core.Application.Features.Concerts.Commands.CreateConcert
+{
+    public class CreateConcertRequestValidator : AbstractValidator<CreateConcertRequest>
+    {
+        public CreateConcertRequestValidator()
+        {
+            RuleFor(p => p.Command.Concert.Name)
+                .NotEmpty().WithMessage("{p.Command.Concert.Name} is required.")
+                .NotNull();
+
+            RuleFor(p => p.Command.Concert.TicketsInSale)
+                .NotEmpty().WithMessage("{p.Command.Concert.TicketsInSale} is required.")
+                .GreaterThan(0).WithMessage("{p.Command.Concert.TicketsInSale} should be greater than zero.");
+        }
+    }
+}
